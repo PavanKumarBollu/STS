6bb73b8ac6fc10b2e0be2d2b0aea4fe744e48611
PavanKumarB <bollupavankumar12@gmail.com>
2025-03-28 02:30:12 +0530

    spring boot project on dependency injection

---
+++ 03_Dependency_Injection_Setter_Con/src/main/java/com/pavan/Application.java
@@ -0,0 +1,46 @@
+package com.pavan;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+	
+	static
+	{
+		System.out.println("Application .class file got loading....\n");
+	}
+	
+	public Application() {
+		System.out.println("Application :: zero param constructor got called ....\n");
+	}
+
+
+    @Bean(value = "date")
+    public LocalDateTime getDate()
+	{
+		System.out.println("Date Object got Created Using the @Bean Annotation\n");
+		return LocalDateTime.now();
+	}
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(Application.class, args);
+		
+		
+		System.out.println("************* Container Started ****************\n");
+		
+		System.out.println("Beans Created By Container :: " + Arrays.toString(context.getBeanDefinitionNames()) + "\n");
+		
+		
+				
+		System.out.println("************* Container Stopped ****************\n");
+		((ConfigurableApplicationContext) context).close();
+	}
+
+}
