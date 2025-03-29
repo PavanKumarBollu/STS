c4888eb684acd65d9d0a5b66b26e51cbb17485e4
PavanKumarB <bollupavankumar12@gmail.com>
2025-03-29 10:27:34 +0530

    spring boot project on Injucting the value to the collection type using the application.properties file

---
+++ 06_Injecting_Collection_Type_Data_Using_ApplicationProperties/src/main/java/com/pavan/Application.java
@@ -0,0 +1,21 @@
+package com.pavan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(Application.class, args);
+		
+		
+		
+		
+		
+		((ConfigurableApplicationContext) context).close();
+	}
+
+}
