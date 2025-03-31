c9c4e57ab462cf064f24f6313dd71eeb226d4cc1
PavanKumarB <bollupavankumar12@gmail.com>
2025-03-31 17:48:59 +0530

    Poject on Spring JDBC and working with callback methods which is provided by the rowMapper ResultSetExtractor

--- 10_Spring_JDBCTemplate_CallBack_Methods/src/main/java/com/pavan/service/StudentMngtServiceImpl.java
+++ 10_Spring_JDBCTemplate_CallBack_Methods/src/main/java/com/pavan/service/StudentMngtServiceImpl.java
@@ -1,10 +1,13 @@
 package com.pavan.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.pavan.bo.StudentBo;
 import com.pavan.dao.IStudentDao;
 import com.pavan.dto.StudentDto;
 
@@ -18,12 +21,25 @@ public class StudentMngtServiceImpl implements IStudentMngtService{
 	
 	@Override
 	public StudentDto getStudentByNo(int sNo) {
-		return null;
+		
+		StudentBo bo = dao.getStudentByNo(sNo);
+		StudentDto dto = new StudentDto();
+		BeanUtils.copyProperties(bo, dto);
+		return dto;
 	}
 
 	@Override
 	public List<StudentDto> getStudentsByNames(String name1, String name2) {
-		return null;
+		List<StudentDto> dtos = new ArrayList<StudentDto>();
+		List<StudentBo> bos = dao.getStudentsByNames(name1, name2);
+		bos.forEach(bo->{
+			StudentDto dto = new StudentDto();
+			BeanUtils.copyProperties(bo, dto);
+			dtos.add(dto);
+		});
+		
+		return dtos;
+		
 	}
 
 	
